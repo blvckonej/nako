f6c0d488455c9f643848673b27dac144ef8c5f2b
Maxim Razin <blvckonej@gmail.com>
2024-02-11 20:56:09 +0300

    finish start page

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
+import Splide from '@splidejs/splide';
 import '../scss/style.scss';
+
+const mySlider = new Splide('#mySlider', {
+    perPage: 2,
+    gap: '20px',
+    pagination: false,
+    breakpoints: {
+        1024: {
+            perPage: 1
+        },
+        768: {
+            // arrows: false,
+            // pagination: true
+        }
+    }
+    
+});
+
+mySlider.mount();
+
+const mySliderNews = new Splide('#mySliderNews', {
+    perPage: 4,
+    gap: '20px',
+    pagination: false,
+    breakpoints: {
+        1400: {
+            perPage: 3
+        },
+        1024: {
+            perPage: 2
+        },
+        768: {
+            perPage: 1,
+            // arrows: false,
+            // pagination: true
+        } 
+    }
+});
+
+mySliderNews.mount();
