6712d925dc11c19c3013d3d6a7ce74f9bb36927d
 support <support@iMac.local>
2024-02-13 17:56:06 +0300

    edit four page

--- src/js/main.js
+++ src/js/main.js
@@ -4,35 +4,88 @@ import '../scss/style.scss';
 
 tabsDefaultInit();
 
-const mySlider = new Splide('#mySlider', {
-    perPage: 2,
-    gap: '20px',
-    pagination: false,
-    breakpoints: {
-        1024: {
-            perPage: 1
+const sliderOne = document.getElementById('mySlider');
+const sliderTwo = document.getElementById('mySliderNews');
+const sliderThree = document.getElementById('mySliderPersonal');
+const sliderFour = document.getElementById('mySliderDocuments');
+
+if (sliderOne) {
+    const mySlider = new Splide('#mySlider', {
+        perPage: 2,
+        gap: '20px',
+        pagination: false,
+        breakpoints: {
+            1024: {
+                perPage: 1
+            },
         },
-    }
+        
+    });
     
-});
+    mySlider.mount();
+}
 
-mySlider.mount();
+if (sliderTwo) {
+    const mySliderNews = new Splide('#mySliderNews', {
+        perPage: 4,
+        gap: '20px',
+        pagination: false,
+        breakpoints: {
+            1400: {
+                perPage: 3
+            },
+            1024: {
+                perPage: 2
+            },
+            768: {
+                perPage: 1
+            } 
+        }
+    });
+    
+    mySliderNews.mount();
+}
 
-const mySliderNews = new Splide('#mySliderNews', {
-    perPage: 4,
-    gap: '20px',
-    pagination: false,
-    breakpoints: {
-        1400: {
-            perPage: 3
-        },
-        1024: {
-            perPage: 2
-        },
-        768: {
-            perPage: 1
-        } 
-    }
-});
+if (sliderThree) {
+    const mySliderPersonal = new Splide('#mySliderPersonal', {
+        perPage: 4,
+        gap: '33px',
+        height: '525px',
+        pagination: false,
+        breakpoints: {
+            1400: {
+                perPage: 3
+            },
+            1024: {
+                perPage: 2
+            },
+            768: {
+                perPage: 1
+            } 
+        }
+    });
 
-mySliderNews.mount();
+    mySliderPersonal.mount();
+}
+
+if (sliderFour) {
+    const mySliderDocuments = new Splide('#mySliderDocuments', {
+        perPage: 4,
+        gap: '33px',
+        height: '525px',
+        pagination: false,
+        breakpoints: {
+            1400: {
+                perPage: 3
+            },
+            1024: {
+                perPage: 2
+            },
+            768: {
+                perPage: 1
+            } 
+        }
+    });
+
+    mySliderDocuments.mount();
+}
