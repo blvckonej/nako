4eb9df7137801d7192d33b27da40192a6c00cbdc
 support <support@iMac.local>
2024-02-15 12:46:40 +0300

    build version 0.1

--- src/js/pages.config.js
+++ src/js/pages.config.js
@@ -6,7 +6,7 @@ const pages = [
     {name: 'brickHouse', path: resolve(__dirname, '/src/pages/brickHouse.html')},
     {name: 'projectHouse', path: resolve(__dirname, '/src/pages/projectHouse.html')},
     {name: 'company', path: resolve(__dirname, '/src/pages/company.html')},
-    {name: 'projectPage', path: resolve(__dirname, '/src/pages/projectPage.html')},
+    {name: 'projectPage', path: resolve(__dirname, '/src/pages/products.html')},
     {name: '404', path: resolve(__dirname, '/src/pages/404.html')},
     {name: 'policy', path: resolve(__dirname, '/src/pages/policy.html')},
     {name: 'order', path: resolve(__dirname, '/src/pages/order.html')},
