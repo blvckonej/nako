28d5492246326e39c42a6aa1eb6d1c6eaa7aa78e
 support <support@iMac.local>
2024-02-15 17:44:42 +0300

    add order page

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const sliderOne = document.getElementById('mySlider');
 const sliderTwo = document.getElementById('mySliderNews');
 const sliderThree = document.getElementById('mySliderPersonal');
 const sliderFour = document.getElementById('mySliderDocuments');
+const carousel = document.getElementById('main-carousel');
 
 if (sliderOne) {
     const mySlider = new Splide('#mySlider', {
@@ -88,4 +89,39 @@ if (sliderFour) {
     });
 
     mySliderDocuments.mount();
+}
+
+if(carousel) {
+    const splide = new Splide( '#main-carousel', {
+        pagination: false,
+        arrows: false,
+      } );
+      
+      var thumbnails = document.getElementsByClassName( 'thumbnail' );
+        var current;
+
+        for ( var i = 0; i < thumbnails.length; i++ ) {
+        initThumbnail( thumbnails[ i ], i );
+        }
+
+        function initThumbnail( thumbnail, index ) {
+        thumbnail.addEventListener( 'click', function () {
+            splide.go( index );
+        } );
+        }
+
+        splide.on( 'mounted move', function () {
+        var thumbnail = thumbnails[ splide.index ];
+
+        if ( thumbnail ) {
+            if ( current ) {
+            current.classList.remove( 'is-active' );
+            }
+
+            thumbnail.classList.add( 'is-active' );
+            current = thumbnail;
+        }
+        } );
+
+      splide.mount();
 }
